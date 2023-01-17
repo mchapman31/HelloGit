c99892987e3295ef09a8fe7fef6695b94642773a
Mark Chapman <freddy_chapman@yahoo.com>
2023-01-16 19:52:27 -0600

    Third

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -16,7 +16,7 @@ public class HelloGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-	    System.out.println("Hello,World!");
+	    System.out.println("Hello, my beautiful World!");
     }
     
 }
